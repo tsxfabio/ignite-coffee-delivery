02f6825bf050dd9d29a87897911a66d08a2c91aa
Fabio Leardini <leardinifabio90@gmail.com>
2022-12-01 01:26:43 -0300

    att header page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 import { HeaderContainer } from './styles'
 
 export function Home() {
@@ -10,6 +11,28 @@ export function Home() {
             Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
             hora
           </span>
+          <div className="iconAndDescriptionHeader">
+            <div className="list1th">
+              <div className="listContainer">
+                <ShoppingCart size={22} weight="fill" className="cartIcon" />
+                <span>Compra simples e segura</span>
+              </div>
+              <div className="listContainer">
+                <Timer size={22} weight="fill" className="timerIcon" />
+                <span>Embalagem mantém o café intacto</span>
+              </div>
+            </div>
+            <div className="list2th">
+              <div className="listContainer">
+                <Package size={22} weight="fill" className="packageIcon" />
+                <span>Embalagem mantém o café intacto</span>
+              </div>
+              <div className="listContainer">
+                <Coffee size={22} weight="fill" className="coffeeIcon" />
+                <span>O café chega fresquinho até você</span>
+              </div>
+            </div>
+          </div>
         </div>
       </HeaderContainer>
     </div>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -16,5 +16,55 @@ export const HeaderContainer = styled.header`
       font-size: 1.25rem;
       margin-top: 1rem;
     }
+
+    .iconAndDescriptionHeader {
+      display: flex;
+      flex-direction: row;
+
+      margin-top: 4.125rem;
+      .list1th,
+      .list2th {
+        width: 50%;
+        .listContainer {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          margin-bottom: 1.25rem;
+          .cartIcon {
+            box-sizing: content-box;
+            border-radius: 50%;
+            padding: 0.5rem;
+            color: ${(props) => props.theme['white-100']};
+            background-color: ${(props) => props.theme['yellow-700']};
+          }
+          .timerIcon {
+            box-sizing: content-box;
+            border-radius: 50%;
+            padding: 0.5rem;
+            color: ${(props) => props.theme['white-100']};
+            background-color: ${(props) => props.theme['yellow-500']};
+          }
+          .packageIcon {
+            box-sizing: content-box;
+            border-radius: 50%;
+            padding: 0.5rem;
+            color: ${(props) => props.theme['white-100']};
+            background-color: ${(props) => props.theme['gray-600']};
+          }
+          .coffeeIcon {
+            box-sizing: content-box;
+            border-radius: 50%;
+            padding: 0.5rem;
+            color: ${(props) => props.theme['white-100']};
+            background-color: ${(props) => props.theme['purple-500']};
+          }
+          span {
+            font-family: 'Roboto', sans-serif;
+            font-size: 1rem;
+            margin: 0 0 0 0.75rem;
+          }
+        }
+      }
+    }
   }
 `
