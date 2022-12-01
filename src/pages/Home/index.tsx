229791786a29839f00b9db17a6a17b858c870e07
Fabio Leardini <leardinifabio90@gmail.com>
2022-11-30 23:37:18 -0300

    att header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,15 @@ import { HeaderContainer } from './styles'
 export function Home() {
   return (
     <div>
-      <HeaderContainer>teste</HeaderContainer>
+      <HeaderContainer>
+        <div className="titleAndDescriptionHeader">
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <span>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </span>
+        </div>
+      </HeaderContainer>
     </div>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
+  padding: 5.875rem 10rem;
   height: 544px;
+
+  .titleAndDescriptionHeader {
+    width: 60%;
+    h1 {
+      font-family: 'Baloo 2', cursive;
+      font-size: 3rem;
+      line-height: 3.875rem;
+    }
+    span {
+      font-family: 'Roboto', sans-serif;
+      font-size: 1.25rem;
+      margin-top: 1rem;
+    }
+  }
 `
