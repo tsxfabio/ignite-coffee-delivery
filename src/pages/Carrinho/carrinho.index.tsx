47d30298dc101b79b6928f94ae1802c5abfd29e6
Fabio Leardini <leardinifabio90@gmail.com>
2022-12-16 01:45:49 -0300

    atualizacao pagina carrinho

--- src/pages/Carrinho/carrinho.index.tsx
+++ src/pages/Carrinho/carrinho.index.tsx
@@ -12,39 +12,62 @@ import { FormEndereco } from './FormEndereco/FormEndereco.index'
 export function Carrinho() {
   return (
     <PageContainer>
-      <Titles title="Complete seu pedido" subtitle={true} />
-      <div className="simpleContainer">
-        <div className="titleContainerEndereco">
-          <MapPinLine size={22} className="iconsEndereco" />
-          <div>
-            <span>Endereço de Entrega</span>
-            <span>Informe o endereço onde deseja receber seu pedido</span>
+      <div className="dadosPedido">
+        <Titles title="Complete seu pedido" subtitle={true} />
+        <div className="simpleContainer">
+          <div className="titleContainerEndereco">
+            <MapPinLine size={22} className="iconsEndereco" />
+            <div>
+              <span>Endereço de Entrega</span>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+          </div>
+          <FormEndereco />
+        </div>
+        <div className="metodoPagamentoContainer">
+          <div className="titlemetodoPagamentoContainer">
+            <CurrencyDollar size={22} className="iconsPagamento" />
+            <div>
+              <span>Pagamento</span>
+              <span>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </span>
+            </div>
+          </div>
+          <div className="cardMetodosPagamento">
+            <div>
+              <CreditCard size={22} className="iconsPagamento" />
+              <span>Cartão de Crédito</span>
+            </div>
+            <div>
+              <Bank size={22} className="iconsPagamento" />
+              <span>Cartão de Débito</span>
+            </div>
+            <div>
+              <Money size={22} className="iconsPagamento" />
+              <span>Dinheiro</span>
+            </div>
           </div>
         </div>
-        <FormEndereco />
       </div>
-      <div className="metodoPagamentoContainer">
-        <div className="titlemetodoPagamentoContainer">
-          <CurrencyDollar size={22} className="iconsPagamento" />
-          <div>
-            <span>Pagamento</span>
-            <span>
-              O pagamento é feito na entrega. Escolha a forma que deseja pagar
-            </span>
-          </div>
-        </div>
-        <div className="cardMetodosPagamento">
-          <div>
-            <CreditCard size={22} className="iconsPagamento" />
-            <span>Cartão de Crédito</span>
-          </div>
-          <div>
-            <Bank size={22} className="iconsPagamento" />
-            <span>Cartão de Débito</span>
-          </div>
-          <div>
-            <Money size={22} className="iconsPagamento" />
-            <span>Dinheiro</span>
+      <div className="confirmacaoPedido">
+        <Titles title="Cafés selecionados" subtitle={true} />
+        <div className="confirmacaoPagamentoContainer">
+          <div></div>
+          <div className="resumoValor">
+            <div>
+              <span>Total de itens</span>
+              <span>R$ 29,70</span>
+            </div>
+            <div>
+              <span>Entrega</span>
+              <span>R$ 3,50</span>
+            </div>
+            <div>
+              <span>Total</span>
+              <span>R$ 33,20</span>
+            </div>
+            <button type="submit">Confirmar Pedido</button>
           </div>
         </div>
       </div>
--- src/pages/Carrinho/carrinho.styles.ts
+++ src/pages/Carrinho/carrinho.styles.ts
@@ -2,102 +2,154 @@ import styled from 'styled-components'
 
 export const PageContainer = styled.main`
   margin-top: 3rem;
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
 
-  .simpleContainer {
+  .dadosPedido {
     display: flex;
     flex-direction: column;
+    max-width: 60%;
 
-    width: 60%;
-    border-radius: 6px;
-    padding: 2.5rem;
-    background-color: ${(props) => props.theme['gray-100']};
-
-    .titleContainerEndereco {
+    .simpleContainer {
       display: flex;
-      flex-direction: row;
-      gap: 0.5rem;
-      margin-bottom: 2rem;
+      flex-direction: column;
 
-      span {
-        display: block;
-        font-family: 'Roboto', sans-serif;
+      width: 100%;
+      border-radius: 6px;
+      padding: 2.5rem;
+      background-color: ${(props) => props.theme['gray-100']};
+
+      .titleContainerEndereco {
+        display: flex;
+        flex-direction: row;
+        gap: 0.5rem;
+        margin-bottom: 2rem;
+
+        span {
+          display: block;
+          font-family: 'Roboto', sans-serif;
+        }
+
+        span:first-child {
+          font-size: 1rem;
+          line-height: 1.3rem;
+        }
+
+        span:last-child {
+          font-size: 0.875rem;
+        }
+
+        .iconsEndereco {
+          color: ${(props) => props.theme['yellow-700']};
+        }
+      }
+    }
+
+    .metodoPagamentoContainer {
+      display: flex;
+      flex-direction: column;
+
+      margin-top: 1rem;
+
+      width: 100%;
+      border-radius: 6px;
+      padding: 2.5rem;
+      background-color: ${(props) => props.theme['gray-100']};
+
+      .titlemetodoPagamentoContainer {
+        display: flex;
+        flex-direction: row;
+        gap: 0.5rem;
+        margin-bottom: 2rem;
+
+        span {
+          display: block;
+          font-family: 'Roboto', sans-serif;
+        }
+
+        span:first-child {
+          font-size: 1rem;
+          line-height: 1.3rem;
+        }
+
+        span:last-child {
+          font-size: 0.875rem;
+        }
+
+        .iconsPagamento {
+          color: ${(props) => props.theme['purple-500']};
+        }
       }
 
-      span:first-child {
-        font-size: 1rem;
-        line-height: 1.3rem;
-      }
+      .cardMetodosPagamento {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
 
-      span:last-child {
-        font-size: 0.875rem;
-      }
+        div {
+          display: flex;
+          flex-direction: row;
+          justify-content: start;
+          align-items: center;
 
-      .iconsEndereco {
-        color: ${(props) => props.theme['yellow-700']};
+          width: 32.5%;
+          gap: 0.25rem;
+          border-radius: 6px;
+          padding: 1rem 0 1rem 1rem;
+
+          font-size: 0.75rem;
+          font-family: 'Roboto', sans-serif;
+          text-transform: uppercase;
+          background-color: ${(props) => props.theme['gray-300']};
+        }
+
+        .iconsPagamento {
+          color: ${(props) => props.theme['purple-500']};
+        }
       }
     }
   }
 
-  .metodoPagamentoContainer {
+  .confirmacaoPedido {
     display: flex;
     flex-direction: column;
+    width: 40%;
 
-    margin-top: 1rem;
+    .confirmacaoPagamentoContainer {
+      background-color: ${(props) => props.theme['gray-100']};
+      border-radius: 6px 44px;
+      padding: 2.5rem;
 
-    width: 60%;
-    border-radius: 6px;
-    padding: 2.5rem;
-    background-color: ${(props) => props.theme['gray-100']};
-
-    .titlemetodoPagamentoContainer {
-      display: flex;
-      flex-direction: row;
-      gap: 0.5rem;
-      margin-bottom: 2rem;
-
-      span {
-        display: block;
-        font-family: 'Roboto', sans-serif;
-      }
-
-      span:first-child {
-        font-size: 1rem;
-        line-height: 1.3rem;
-      }
-
-      span:last-child {
-        font-size: 0.875rem;
-      }
-
-      .iconsPagamento {
-        color: ${(props) => props.theme['purple-500']};
-      }
-    }
-
-    .cardMetodosPagamento {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-
-      div {
+      .resumoValor {
         display: flex;
-        flex-direction: row;
-        justify-content: start;
-        align-items: center;
+        flex-direction: column;
+        gap: 0.75rem;
 
-        width: 32.5%;
-        gap: 0.25rem;
-        border-radius: 6px;
-        padding: 1rem 0 1rem 1rem;
+        div {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
 
-        font-size: 0.75rem;
-        font-family: 'Roboto', sans-serif;
-        text-transform: uppercase;
-        background-color: ${(props) => props.theme['gray-300']};
-      }
+        button {
+          width: 100%;
+          padding: 0.75rem;
+          border-radius: 6px;
+          border: 1px solid transparent;
+          cursor: pointer;
 
-      .iconsPagamento {
-        color: ${(props) => props.theme['purple-500']};
+          font-weight: 700;
+          text-transform: uppercase;
+          transition: all 0.2s ease-in-out;
+
+          color: ${(props) => props.theme['white-100']};
+          background-color: ${(props) => props.theme['yellow-500']};
+
+          :hover {
+            background-color: ${(props) => props.theme['yellow-700']};
+          }
+        }
       }
     }
   }
