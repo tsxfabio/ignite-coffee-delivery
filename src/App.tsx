2279af1159657158a01e5232b8717d2acc4e6b86
Fabio Leardini <leardinifabio90@gmail.com>
2023-01-07 00:16:01 -0300

    iniciando context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
-import { Header } from './components/Header'
+import { CoffeeContextProvider } from './contexts/CoffeeContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CoffeeContextProvider>
+          <Router />
+        </CoffeeContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
